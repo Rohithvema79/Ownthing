6057a872d6090eab26f32d3d2e6ea2b99f875570
Rohith Vema <rohithvema09@gmail.com>
2025-09-15 20:55:05 +0530

    Revert "Jenkins"
    This reverts commit da4855f03bc9f8c6b028081b48319e02c104c5da.

--- Vehicles/src/main/java/com/klef/VehiclesApplication.java
+++ Vehicles/src/main/java/com/klef/VehiclesApplication.java
@@ -2,10 +2,9 @@ package com.klef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VehiclesApplication extends SpringBootServletInitializer {
+public class VehiclesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehiclesApplication.class, args);
